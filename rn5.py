f1cbd4a2bfa4b1a28dfa732c0b3fd27c11e21db9
Kaua <kakabuchweitz@gmail.com>
2023-05-23 15:00:47 -0300

    correcoes

--- rn5.py
+++ rn5.py
@@ -3,10 +3,21 @@ import pandas as pd
 
 dados = pd.read_csv("projetos/projeto2dsa/dataset.csv")
 
-df5 = dados.groupby("Segmento")["Valor_Venda"].sum()
-print(df5)
-color = ["k", "c", "pink"]
+df5 = dados.groupby("Segmento")[
+    "Valor_Venda"].sum().sort_values(ascending=False)
 
-plt.pie(df5, colors=color, shadow=True, labels=df5.keys())
+
+def autopct_format(values):
+    def my_format(pct):
+        total = sum(values)
+        val = int(round(pct * total / 100.0))
+        return ' $ {v:d}'.format(v=val)
+    return my_format
+
+
+color = ["r", "c", "pink"]
+
+plt.pie(df5, colors=color, shadow=True, labels=df5.keys(),
+        autopct=autopct_format(df5.values))
 
 plt.show()
